27cd32b8e60a3b3b7a04baa0f03698aaf0684a8e
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-01-23 16:04:39 +0900

    update lower_bound problem

--- chapter6/pair_opt.py
+++ chapter6/pair_opt.py
@@ -1,14 +1,31 @@
 # code 6.4
 
+import bisect
+
+INF = 20_000_000
+
 N = int(input())
 K = int(input())
 
 a = [int(input()) for _ in range(N)]
 b = [int(input()) for _ in range(N)]
 
-min_value = float("inf")
+min_value = INF
 
 b.sort()
 
-b.append(float("inf"))
+b.append(INF)
 
+for i in range(N):
+    # std::lower_bound の代わりに Python では bisect.bisect_left が使用できる。
+    # この関数は C++ とは違い、イテレータではなくインデックスを返すので、
+    # index という変数名に変更している。
+    index = bisect.bisect_left(b, K - a[i])
+
+    # 挿入可能な位置の値を取り出す
+    val = b[index]
+
+    if (a[i] + val < min_value):
+        min_value = a[i] + val
+
+print(min_value)
