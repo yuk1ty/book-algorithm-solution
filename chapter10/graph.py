0d2707296a63e8fd89de8add1d234656fd5f4b8f
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-01-26 01:55:20 +0900

    add graph implementation

---
+++ chapter10/graph.py
@@ -0,0 +1,5 @@
+from typing import TypeVar, List
+
+T = TypeVar("T")
+
+Graph = List[List[T]]
---
+++ chapter10/input_graph.py
@@ -0,0 +1,14 @@
+# code 10.3
+
+from typing import List
+
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+
+    G[a].append(b)
+
+    # 無向グラフなら
+    # G[b].append(a)
