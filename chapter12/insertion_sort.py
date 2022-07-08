ea3833dd1a4a62dd3b28d6d7f78c5de37d065e8d
yuk1ty <yuk1ty@users.noreply.github.com>
2022-07-09 00:33:09 +0900

    tab to space

--- chapter12/insertion_sort.py
+++ chapter12/insertion_sort.py
@@ -1,18 +1,18 @@
 from typing import List
 
 def insertion_sort(a: List[int]):
-	N = len(a)
-	for i in range(N):
-		v = a[i]
-	
-		j = i
-		while j > 0:
-			if a[j-1] > v:
-				a[j] = a[j - 1]
-				j -= 1
-			else:
-				break
-		a[j] = v
+    N = len(a)
+    for i in range(N):
+        v = a[i]
+
+        j = i
+        while j > 0:
+            if a[j-1] > v:
+                a[j] = a[j - 1]
+                j -= 1
+            else:
+                break
+        a[j] = v
 
 N = int(input())
 a = list(map(int, input().split()))
