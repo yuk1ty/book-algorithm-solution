c774e7028d8563d75656ff39a2d126237570ae97
yuk1ty <yuk1ty@users.noreply.github.com>
2022-07-09 00:31:01 +0900

    add insertion sort

---
+++ chapter12/insertion_sort.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def insertion_sort(a: List[int]):
+	N = len(a)
+	for i in range(N):
+		v = a[i]
+	
+		j = i
+		while j > 0:
+			if a[j-1] > v:
+				a[j] = a[j - 1]
+				j -= 1
+			else:
+				break
+		a[j] = v
+
+N = int(input())
+a = list(map(int, input().split()))
+
+insertion_sort(a)
+print(a)
